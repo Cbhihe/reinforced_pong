f5a6489a210c15c5519ade5ee7fd4ca711b81967
Rodrigo <rodarima@gmail.com>
2018-04-16 18:04:31 +0200

    Add human play

--- src/test.py
+++ src/test.py
@@ -1,7 +1,7 @@
 # Encoding: utf-8
 
-import pygame, os
-from pygame.sprite import Sprite
+import pygame, os, time
+from pygame.sprite import Sprite, collide_rect
 from pygame import Rect,Surface
 
 # Center window
@@ -10,93 +10,230 @@ os.environ['SDL_VIDEO_CENTERED'] = '1'
 BOARD_MARGIN_X = 20
 
 PADDLE_MARGIN_X = 50
-PADDLE_MARGIN_Y = 30
+PADDLE_MARGIN_Y = 10
 
 PADDLE_WIDTH = 10
 PADDLE_HEIGHT = 100
+
+BALL_DIAMETER = 10
+
+DRAW = True
+FPS = 60
 
 class Board(Sprite):
 	def __init__(self, screen, size):
 		Sprite.__init__(self)
 		self.screen = screen
-		self.size = size
-		self.board = Surface(self.size)
-		self.init_paddles()
+		self.surf = Surface(size)
+		self.rect = self.surf.get_rect()
 	
-	def init_paddles(self):
-		w,h = self.size
+		self.pl = Paddle(self.surf, PADDLE_MARGIN_X, PADDLE_MARGIN_Y, 'left')
+		self.pr = Paddle(self.surf, PADDLE_MARGIN_X, PADDLE_MARGIN_Y, 'right')
 
-		self.pl = Paddle(self.board, PADDLE_MARGIN_X, PADDLE_MARGIN_Y, 'left')
-		self.pr = Paddle(self.board, PADDLE_MARGIN_X, PADDLE_MARGIN_Y, 'right')
+		self.ball = Ball(self, BALL_DIAMETER)
+
+		self.cl = PaddleControllerFollower(self, self.pl, self.ball)
+		self.cr = PaddleControllerKeyboard(self, self.pr, self.ball)
+
+	def draw_net(self):
+		rect = self.surf.get_rect()
+		top = rect.midtop
+		bottom = rect.midbottom
+		pygame.draw.line(self.surf, (150,150,150), top, bottom, 1)
 	
 	def update(self):
+		self.ball.update()
+
+		self.cl.update()
+		self.cr.update()
+
 		self.pl.update()
 		self.pr.update()
-		self.screen.blit(self.board, dest=(0,0))
+
+	def draw(self):
+		# Clear the board
+		self.surf.fill((0,0,0))
+
+		# Draw elements
+		self.draw_net()
+		self.ball.draw()
+
+		self.pl.draw()
+		self.pr.draw()
+
+		# Finally blit into the screen
+		self.screen.blit(self.surf, dest=(0,0))
+
+
+class PaddleControllerFollower:
+	def __init__(self, board, paddle, ball):
+		self.board = board
+		self.paddle = paddle
+		self.ball = ball
+
+	def update(self):
+
+		bx,by = self.ball.position
+		py = self.paddle.y
+
+		if py < by:
+			vy = self.paddle.top_speed
+		elif py > by:
+			vy = -self.paddle.top_speed
+		else:
+			vy = 0
+	
+		self.paddle.set_speed(vy)
+
+class PaddleControllerKeyboard:
+	def __init__(self, board, paddle, ball):
+		self.board = board
+		self.paddle = paddle
+		self.ball = ball
+
+	def update(self):
+
+		vy = self.paddle.vy
+		events = pygame.event.get()
+		for event in events:
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_UP:
+					vy = -self.paddle.top_speed
+				if event.key == pygame.K_DOWN:
+					vy = self.paddle.top_speed
+			elif event.type == pygame.KEYUP:
+				vy = 0
+	
+		self.paddle.set_speed(vy)
+
 
 class Paddle(Sprite):
 	def __init__(self, board, hmargin, vmargin, side):
 		Sprite.__init__(self)
 		self.board = board
 		self.board_size = board.get_size()
+		self.margin = (hmargin, vmargin)
+
 		self.paddle_size = (PADDLE_WIDTH, PADDLE_HEIGHT)
+		self.rect = Rect((0, 0), self.paddle_size)
+		w,h = self.board_size
+
+		# Place the paddle at the top
+		if side == 'left':
+			self.rect.topleft = (hmargin, vmargin)
+		else:
+			self.rect.topright = (w - hmargin, vmargin)
 
 		wboard, hboard = self.board_size
 		wsurface = PADDLE_WIDTH
 		hsurface = hboard - vmargin*2
-
-		self.surf_size = (wsurface, hsurface)
-
-		# Position is a float from [0,1] with 0 the top, and 1 the bottom.
-		self.position = 0.0
-
-		self.surf = Surface(self.surf_size)
-		self.surf_rect = Rect((0, 0), self.surf_size)
-		w,h = self.board_size
-
-		if side == 'left':
-			self.surf_rect.topleft = (hmargin, vmargin)
-		else:
-			self.surf_rect.topright = (w - hmargin, vmargin)
-
-		paddle_size = (PADDLE_WIDTH, PADDLE_HEIGHT)
-		self.paddle = Surface(paddle_size)
-		self.paddle.fill((255,255,255))
-		self.paddle_rect = Rect((0, 0), paddle_size)
+		surf_size = (wsurface, hsurface)
+		self.surf = Rect(self.rect.topleft, surf_size)
 
 		self.side = side
-		if side == 'left':
-			self.x = 0
-		else:
-			self.x = w
+
+		# The center y position of the paddle in floating point
+		self.y = self.rect.centery
+
+		# Paddle vertical speed
+		self.vy = 0.0
+
+		self.top_speed = 3.0
+
+	def set_speed(self, vy):
+		if vy > self.top_speed:
+			vy = self.top_speed
+		if vy < -self.top_speed:
+			vy = -top_speed
+
+		self.vy = vy
+
+	def set_position(self, y):
+		rect = self.rect
+
+		rect.centery = y
+		top = self.surf.top
+		bottom = self.surf.bottom
+
+		if rect.top < top:
+			rect.top = top
+			y = rect.centery
+		if rect.bottom > bottom:
+			rect.bottom = bottom
+			y = rect.centery
+
+		self.y = y
 
 	def update(self):
-		incr = 0.02
-		if self.side == 'left':
-			self.position += incr
-		else:
-			self.position -= incr * 0.7
+		self.set_position(self.y + self.vy)
 
-		if self.position > 1.0:
-			self.position = 0.0
-		elif self.position < 0.0:
-			self.position = 1.0
+	def draw(self):
+		# Draw the paddle in the rect position
+		self.board.fill((255,255,255), self.rect)
 
-		sw,sh = self.surf_size
-		pw,ph = self.paddle_size
 
-		x = 0
-		y = self.position * (sh - ph)
-		self.paddle_rect.topleft = (x, y)
+class Ball(Sprite):
+	def __init__(self, board, diameter):
+		Sprite.__init__(self)
+		self.board = board
+		self.diameter = diameter
+		self.size = (diameter, diameter)
+		self.ball = Surface(self.size)
+		self.ball.fill((0,255,0))
+		self.rect = Rect((0, 0), self.size)
 
-		# Clear previos paddle
-		self.surf.fill((100,0,0))
+		w,h = self.board.surf.get_size()
 
-		# Place the paddle into the paddle surface, in the correct position
-		self.surf.blit(self.paddle, self.paddle_rect)
+		# The ball center position in floating point
+		self.position = (w/2, h/2)
 
-		# Then place the updated paddle surface into the board
-		self.board.blit(self.surf, self.surf_rect)
+		# Speed is measured in pixels / frame
+		self.speed = (3.0, 3.3)
+
+	def check_miss(self):
+		# Test if the ball was missed by any paddle
+
+		x, y = self.rect.center
+
+		pr = self.board.pr
+
+
+
+	def collide_paddles(self):
+		pr = self.board.pr
+		pl = self.board.pl
+
+		vx, vy = self.speed
+
+		if collide_rect(self, pr) or collide_rect(self, pl):
+			vx = -vx
+
+		self.speed = (vx, vy)
+
+
+	def update(self):
+		w,h = self.board.surf.get_size()
+
+		self.collide_paddles()
+
+		px, py = self.position
+		vx, vy = self.speed
+		px += vx
+		py += vy
+
+		if px > w or px < 0:
+			vx = -vx
+
+		if py > h or py < 0:
+			vy = -vy
+
+		self.speed = (vx, vy)
+		self.position = (px, py)
+
+		self.rect.center = (px, py)
+
+	def draw(self):
+		self.board.surf.blit(self.ball, self.rect)
 
 
 pygame.init()
@@ -106,9 +243,26 @@ size = (320*2, 240*2)
 b = Board(screen, size)
 clock = pygame.time.Clock()
 
+frame = 1
+delay = 10000
+
+tic = time.time()
 while True:
 	b.update()
-	pygame.display.update()
-	pygame.display.flip()
 
-	clock.tick(60)
+	if DRAW:
+		b.draw()
+		pygame.display.update()
+		pygame.display.flip()
+		clock.tick(FPS)
+		delay = 200
+
+	if not frame % delay:
+		toc = time.time()
+		fps = frame/(toc-tic)
+		#print("FPS = {:.2f}".format(clock.get_fps()))
+		print("Real fps = {:.2f}".format(fps))
+
+	frame += 1
+
+	
